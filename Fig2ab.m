0c1163311fc6c506848a3d438474a38fa3f421de
RenZhangPhy <132209967+RenZhangPhy@users.noreply.github.com>
2023-05-02 11:38:12 +0800

    update

---
+++ Fig2ab.m
@@ -0,0 +1,115 @@
+
+%%
+% Fig 2 a and b
+
+figure('Position',[900 200 1700 600])
+hold on
+box on
+
+dataC1 = load('Correlation_Ozx_site=13_O_loc=6_h=1_g=0.txt');
+databn1 = load('bn_Ozx_site=13_O_loc=6_h=1_g=0.txt');
+
+dataC2 = load('Correlation_Oyy_site=13_O_loc=6_h=1_g=1.txt');
+databn2 = load('bn_Oyy_site=13_O_loc=6_h=1_g=1.txt');
+
+
+fitn = 4;
+fitend = 10;
+
+subplot(1,2,1)
+hold on
+box on
+
+plot(databn1(:,1),databn1(:,2),'MarkerSize',10,'Marker','o','LineWidth',1.5,'LineStyle','none','Color','r')
+plot(databn2(:,1),databn2(:,2),'MarkerSize',10,'Marker','s','LineWidth',1.5,'LineStyle','none','Color','b')
+
+p = polyfit(databn1(1:fitend,1),databn1(1:fitend,2),fitn);
+derp = polyder(p);
+derbn1 = polyval(derp,1)/polyval(p,1);%b'[1]/b[1]
+databnfit1 = polyval(p,1:0.1:fitend);
+plot(1:0.1:fitend,databnfit1,'LineWidth',2,'LineStyle','-','Color','r')
+
+
+p = polyfit(databn2(1:fitend,1),databn2(1:fitend,2),fitn);
+derp = polyder(p);
+derbn2 = polyval(derp,1)/polyval(p,1);%b'[1]/b[1]
+databnfit2 = polyval(p,1:0.1:fitend);
+plot(1:0.1:fitend,databnfit2,'LineWidth',2,'LineStyle','-','Color','b')
+
+set(gca,'FontSize',40,'LineWidth',2,'xlim',[0,20],'ylim',[0,15])
+xlabel('$n$','interpreter','latex','fontsize',60)
+ylabel('$b_n$','interpreter','latex','fontsize',60)
+
+
+fitendarr = 5:15;
+num = size(fitendarr);
+
+for i = 1:num(2)
+
+    fitend = fitendarr(i);
+    p1 = polyfit(databn1(1:fitend,1),databn1(1:fitend,2),fitn);
+    derp1 = polyder(p1);
+    derbn1(i) = polyval(derp1,1)/polyval(p1,1);%b'[1]/b[1]
+    
+    
+    p2 = polyfit(databn2(1:fitend,1),databn2(1:fitend,2),fitn);
+    derp2 = polyder(p2);
+    derbn2(i) = polyval(derp2,1)/polyval(p2,1);%b'[1]/b[1]
+
+end
+
+leg = legend([num2str(mean(derbn1),'%.3f'),'$\pm$',num2str(std(derbn1)/sqrt(num(2)),'%.3f'),'$,\sigma_0^z\sigma_1^x$'],...
+    [num2str(mean(derbn2),'%.3f'),'$\pm$',num2str(std(derbn2)/sqrt(num(2)),'%.3f'),'$,\sigma_0^y\sigma_1^y$']);
+set(leg,'Interpreter','latex','Location','northwest','FontSize',40,'box','off');
+
+
+subplot(1,2,2)
+hold on
+box on
+
+plot(dataC1(:,1)*databn1(1,2),dataC1(:,2),'MarkerSize',10,'Marker','o','LineWidth',1,'LineStyle','-','Color','r')
+plot(dataC2(:,1)*databn2(1,2),dataC2(:,2),'MarkerSize',10,'Marker','s','LineWidth',1,'LineStyle','-','Color','b')
+
+
+set(gca,'FontSize',40,'LineWidth',2,'xlim',[0,5])
+xlabel('$tb_1$','interpreter','latex','fontsize',60)
+ylabel('${\cal C}(t)$','interpreter','latex','fontsize',60)
+
+
+
+axes('Position',[0.687058823529412 0.568333333333333 0.207764705882343 0.329047619047625]);
+hold on
+box on
+
+data = load('Correlation_Ozx_site=13_O_loc=6_h=1_g=0.txt');
+plot(data(:,1),data(:,2),'MarkerSize',10,'Marker','o','LineWidth',1,'LineStyle','-','Color','r')
+
+data = load('Corr_Expolate_Oyy_h=1_g=1.txt');
+plot(data(:,1),data(:,2),'MarkerSize',10,'Marker','s','LineWidth',1,'LineStyle','-','Color','b')
+
+
+set(gca,'FontSize',40,'LineWidth',2,'xlim',[0,2])
+xlabel('$tJ$','interpreter','latex','fontsize',50)
+% ylabel('$C(t)$','interpreter','latex','fontsize',50)
+
+% Create textbox
+annotation('textbox',...
+    [0.134666666666666 0.25 0.0413333333333334 0.0850000000000001],...
+    'String','(a)',...
+    'LineStyle','none',...
+    'Interpreter','latex',...
+    'FontSize',60,...
+    'FontName','Helvetica Neue',...
+    'FitBoxToText','off');
+
+% Create textbox
+annotation('textbox',...
+    [0.577333333333332 0.25 0.0413333333333342 0.0833333333333338],...
+    'String','(b)',...
+    'LineStyle','none',...
+    'Interpreter','latex',...
+    'FontSize',60,...
+    'FontName','Helvetica Neue',...
+    'FitBoxToText','off');
+
+print('Universal_Ising','-depsc')
